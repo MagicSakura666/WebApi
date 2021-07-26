00b8defc2c933b5fb2b5135b8e9e0419cca7c797
残风飞雪 <849100182@qq.com>
2021-07-26 10:09:40 +0800

    11

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing07261006", "Bracing", "Chilly"
+            "Freezing07261009", "Bracing", "Chilly"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
